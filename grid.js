d93d7be65c4463ef5998bd1639101f31bedd94b8
sunnycow <sannikov_yao@farpost.com>
2026-03-18 11:29:34 +1000

    Add grid resize on resize button click

--- grid.js
+++ grid.js
@@ -2,6 +2,8 @@ const SQUARE_SIDE_LENGTH = 960;
 const container = document.querySelector('#container');
 
 function createGrid(size) {
+  container.innerHTML = '';
+
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
 
@@ -15,3 +17,20 @@ function createGrid(size) {
 }
 
 createGrid(16);
+
+const resizeButton = document.querySelector('#resize')
+
+resizeButton.addEventListener('click', () => {
+  const input = prompt('Enter a number from 1 to 100');
+
+  if (input === null) return;
+
+  const size = Number(input);
+
+  if (!Number.isInteger(size) || size < 1 || size > 100) {
+    alert('ERROR!');
+    return;
+  }
+
+  createGrid(size);
+});
